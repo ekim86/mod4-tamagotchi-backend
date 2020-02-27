c6d2ac5144660bf6ddcfa6f573857a0ce9dc7090
flatironschool <danliu277@gmail.com>
2020-02-27 11:44:02 -0500

    Fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,6 @@ User.destroy_all
 
 user1 = User.create(username: 'name1', password: '123')
 tamagotchi1 = Tamagotchi.create(name: 'tname1', species: 'species1', happiness: 2, fullness:1, income: 3, image: 'https://66.media.tumblr.com/7e4951d895d1b4db1f2d2ea6ba89b686/tumblr_oui6v5rcM51ws4s25o1_500.gif')
-item1 = Item.create(name: 'iname1', effect: 2, category: 'food', price: 5)
-inventory1 = Inventory.create(user: user1, item: item1, quantity: 3, image: 'https://i1.pngguru.com/preview/313/486/450/25-kawaii-food-s-sushi-illustration.jpg')
+item1 = Item.create(name: 'iname1', effect: 2, category: 'food', price: 5, image: 'https://i1.pngguru.com/preview/313/486/450/25-kawaii-food-s-sushi-illustration.jpg')
+inventory1 = Inventory.create(user: user1, item: item1, quantity: 3)
 status1 = Status.create(user: user1, tamagotchi: tamagotchi1, happiness: 10, fullness: 10, money: 100)
