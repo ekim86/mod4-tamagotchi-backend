bc01a65a7a53e0c8f348be5df1aeab43bcc49cfd
flatironschool <danliu277@gmail.com>
2020-02-28 14:17:54 -0500

    Add play and feed

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,51 +1,82 @@
 class InventoriesController < ApplicationController
+  before_action :get_info, only: [:buy, :sell, :feed, :play]
+
+  def get_info
+    @inventory_params = inventories_params
+    @status = Status.find(@inventory_params['status_id'])
+    @item = Item.find(@inventory_params['item_id'])
+    @inventory = nil
+  end
+
+  def reduce
+    @inventory = Inventory.find_by(status_id: @inventory_params['status_id'], item_id: @inventory_params['item_id'])
+
+    if @inventory && @inventory.quantity > 1
+      @inventory.quantity -= 1
+      @inventory.save
+    else
+      @inventory.quantity = 0
+      @inventory.destroy
+    end
+  end
+
   def index
     render json: Status.find(params[:status_id]).inventories
   end
 
   def buy
-    inventory_params = inventories_params
-    status = Status.find(inventory_params['status_id'])
-    item = Item.find(inventory_params['item_id'])
-    inventory = nil
-    
-    # Check if user has enough moneu
-    if status.money >= item.price
-      inventory = Inventory.find_by(status_id: inventory_params['status_id'], item_id: inventory_params['item_id'])
-      if inventory
-        inventory.quantity += 1
-        inventory.save
+    # Check if user has enough money
+    if @status.money >= @item.price
+      @inventory = Inventory.find_by(status_id: @inventory_params['status_id'], item_id: @inventory_params['item_id'])
+      if @inventory
+        @inventory.quantity += 1
+        @inventory.save
       else
-        inventory = Inventory.create(inventory_params)
+        @inventory = Inventory.create(@inventory_params)
       end
-      status.money -= item.price
-      status.save
+      @status.money -= @item.price
+      @status.save
     end
 
     render json: {
-      inventory: InventorySerializer.new(inventory),
-      money: status.money
+      inventory: InventorySerializer.new(@inventory),
+      money: @status.money
     }
   end
 
   def sell
-    inventory_params = inventories_params
-    status = Status.find(inventory_params['status_id'])
-    item = Item.find(inventory_params['item_id'])
-    inventory = Inventory.find_by(status_id: inventory_params['status_id'], item_id: inventory_params['item_id'])
-    if inventory && inventory.quantity > 1
-      inventory.quantity -= 1
-      inventory.save
-    else
-      inventory.quantity = 0
-      inventory.destroy
-    end
-    status.money += item.price
-    status.save
+    reduce
+
+    @status.money += @item.price
+    @status.save
     
     render json: {
-      inventory: InventorySerializer.new(inventory),
-      money: status.money
+      inventory: InventorySerializer.new(@inventory),
+      money: @status.money
+    }
+  end
+
+  def feed
+    reduce
+
+    @status.fullness += @item.effect
+    @status.save
+
+    render json: {
+      inventory: InventorySerializer.new(@inventory),
+      fullness: @status.fullness
+    }
+  end
+
+  def play
+    reduce
+
+    @status.happiness += @item.effect
+    @status.save
+
+    render json: {
+      inventory: InventorySerializer.new(@inventory),
+      happiness: @status.happiness
     }
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
   get 'tamagotchis/', to: 'tamagotchis#index'
   get 'tamagotchis/:id', to: 'tamagotchis#show'
+
   get 'statuses/', to: 'statuses#index'
   post 'statuses/', to: 'statuses#create'
   get 'statuses/:id', to: 'statuses#show'
   patch 'statuses/:id', to: 'statuses#update'
   delete 'statuses/:id', to: 'statuses#destroy'
+
   get 'items/', to: 'items#index'
   get 'items/:id', to: 'items#show'
+
   get 'inventories/:status_id', to: 'inventories#index'
   post 'inventories/buy', to: 'inventories#buy'
   post 'inventories/sell', to: 'inventories#sell'
+  post 'inventories/feed', to: 'inventories#feed'
+  post 'inventories/play', to: 'inventories#play'
+
   post 'users/create', to: 'users#create'
   post 'users/login', to: 'users#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
