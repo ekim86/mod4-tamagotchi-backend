aca335a392466910293c82b5931d7ca3b4b6ae93
flatironschool <danliu277@gmail.com>
2020-03-02 11:37:52 -0500

    Add whenever to decrease status every minute

--- app/models/status.rb
+++ app/models/status.rb
@@ -3,4 +3,16 @@ class Status < ApplicationRecord
     belongs_to :tamagotchi
     has_many :inventories
     has_many :items, through: :inventories
+
+    def self.decrease_all
+        self.all.each do |tama|
+            if tama.happiness > 0
+                tama.happiness -= 1
+            end
+            if tama.fullness > 0
+                tama.fullness -= 1
+            end
+            tama.save
+        end
+    end
 end
